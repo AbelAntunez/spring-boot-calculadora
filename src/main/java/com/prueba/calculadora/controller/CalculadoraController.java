4c94657b47e8755517d0afc475f29be3d4cce95f
abelAntunez <abel.ronin@gmail.com>
2022-01-18 14:16:33 +0100

    [ADDED] Implementado uso de CalculadoraService en CalculadoraController

--- src/main/java/com/prueba/calculadora/controller/CalculadoraController.java
+++ src/main/java/com/prueba/calculadora/controller/CalculadoraController.java
@@ -1,6 +1,9 @@
 package com.prueba.calculadora.controller;
 
+import com.prueba.calculadora.service.CalculadoraService;
+import io.corp.calculator.TracerImpl;
 import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/calculadora")
 public class CalculadoraController {
 
+  @Autowired
+  private CalculadoraService calculadoraService;
+
+  private TracerImpl tracer = new TracerImpl();
+
   @GetMapping("/calcula")
   public ResponseEntity<?> calcula(@RequestParam(name = "primero") BigDecimal primerNumero,
                                     @RequestParam(name = "segundo") BigDecimal segundoNumero,
                                     @RequestParam() String operacion) {
-    return ResponseEntity.ok("1: " + primerNumero + " 2: " + segundoNumero + " operacion: " + operacion);
+    BigDecimal result = calculadoraService.calcula(primerNumero, segundoNumero, operacion);
+    tracer.trace(result);
+
+    return ResponseEntity.ok(result);
   }
 }
